06a7782d02cad97ad3c6443ddbd94783cf6b8f47
jessiacost <jessiacost1996@gmail.com>
2019-06-25 19:50:30 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const {crearArchivo} = require ('./multiplicar/multiplicar');
+
+let base = '10';
+
+
+  crearArchivo(base)
+  .then( archivo => console.log(`Archivo creado: ${ archivo}`) )
+  .catch(e => console.log(e));
